90f2eb9527d7bd6d3dbb7e1186449654df12791f
vetumac <alexei.suslov@instinctools.ru>
2016-02-17 10:01:24 +0300

    +- def

--- src/SquareMatrix.scala
+++ src/SquareMatrix.scala
@@ -18,8 +18,16 @@ case class SquareMatrix(matrix: List[List[Double]]) extends AbstractMatrix(matri
   private def determinantCalc(matrix: List[List[Double]]): Double = {
     matrix.length match {
       case 1 => matrix.head.head
-      case _ => matrix.head.foldLeft(0, 0.0)((r: (Int, Double), cur: Double) => (r._1 + 1, ((-1) ^ r._1) * cur *
+      case _ => matrix.head.foldLeft(0, 0.0)((r: (Int, Double), cur: Double) => (r._1 + 1, (plusMinusOne(r._1)) * cur *
         determinantCalc(matrix.tail.map(ff => List(ff.take(r._1), ff.drop(r._1 + 1)).flatten))))._2
+    }
+  }
+
+
+  def plusMinusOne(j: Int): Int = {
+    j % 2 match {
+      case 0 => 1
+      case 1 => -1
     }
   }
 }
