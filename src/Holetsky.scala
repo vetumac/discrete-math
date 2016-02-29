4a936b88c55589e9554336903088f839c3e21964
vetumac <alexei.suslov@instinctools.ru>
2016-02-29 11:33:57 +0300

    addNotDiogonal

--- src/Holetsky.scala
+++ src/Holetsky.scala
@@ -6,6 +6,8 @@ object Holetsky {
 
   private def firstStep(a: SquareMatrix, l: List[List[Double]]): SquareMatrix = {
     val extL = extendMatrix(l)
+    //extL.last.map()
+    (0 -> (extL.length - 1))
 
 
     //l.matrix.head.head
@@ -14,5 +16,15 @@ object Holetsky {
 
   private def extendMatrix(matrix: List[List[Double]]): List[List[Double]] = {
     matrix.map(f => f :+ 0.0) :+ List()
+  }
+
+  private def addNotDiogonal(a: SquareMatrix, l: List[List[Double]], lLast: List[Double]): List[List[Double]] = {
+    val lAdd = l :+ lLast
+    val i = lAdd.length - 1
+    val j = lAdd.length - 1
+    j > i match {
+      case true => addNotDiogonal(a, l, lLast :+ ((1 / lAdd(i)(i)) * (a.matrix(i)(j) - List.range(0, j - 1).map(k => l(i)(k) * l(j)(k)).sum)))
+      case false => lAdd
+    }
   }
 }
