bcc9099b528e9473ee36aad9113ec5f7ac84a91a
Alexey Suslov <alexei.suslov@gmail.com>
2016-04-28 22:05:41 +0300

    fix bug in devuvatives

--- src/main/scala/by/bsuir/dm/diff/Deriv.scala
+++ src/main/scala/by/bsuir/dm/diff/Deriv.scala
@@ -14,7 +14,7 @@ object Deriv {
         nearlyPoint match {
           case 0 =>
             val y = (0 to 4).map(i => {
-              evaluator.putVariable("x", (i * h).toString)
+              evaluator.putVariable("x", (a + i * h).toString)
               evaluator.evaluate(func).toDouble
             })
             val firstDiff = (-3 * y(0) + 4 * y(1) - y(2)) / (2 * h)
@@ -22,15 +22,15 @@ object Deriv {
             (firstDiff, secondDiff)
           case `n` =>
             val y = (n - 3 to n).map(i => {
-              evaluator.putVariable("x", (i * h).toString)
+              evaluator.putVariable("x", (a + i * h).toString)
               evaluator.evaluate(func).toDouble
             })
             val firstDiff = (y(1) - 4 * y(2) + 3 * y(3)) / (2 * h)
             val secondDiff = (-y(0) + 4 * y(1) - 5 * y(2) + 2 * y(3)) / math.pow(h, 2)
             (firstDiff, secondDiff)
           case _ =>
-            val y = (n - 1 to n + 1).map(i => {
-              evaluator.putVariable("x", (i * h).toString)
+            val y = (nearlyPoint - 1 to nearlyPoint + 1).map(i => {
+              evaluator.putVariable("x", (a + i * h).toString)
               evaluator.evaluate(func).toDouble
             })
             val firstDiff = (-y(0) + y(2)) / (2 * h)
