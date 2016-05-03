730c9474403161ee814363703a25540aacb97b5a
Alexey Suslov <alexei.suslov@gmail.com>
2016-05-03 23:59:23 +0300

    cubic in progress

---
+++ src/main/scala/by/bsuir/dm/aprox/CubicSpline.scala
@@ -0,0 +1,8 @@
+package by.bsuir.dm.aprox
+
+object CubicSpline {
+  def apply(x: Double, points: List[(Double, Double)]): Double = {
+    //points.map()
+    2.0
+  }
+}
--- src/main/scala/by/bsuir/dm/aprox/Lagrange.scala
+++ src/main/scala/by/bsuir/dm/aprox/Lagrange.scala
@@ -4,10 +4,10 @@ object Lagrange {
   def apply(x: Double, points: List[(Double, Double)]): Double = {
     points.map(point => {
       point._2 * points.foldLeft(1.0)((acc, these) =>
-      point._1 == these._1 match {
-        case true => acc
-        case false => acc * (x - these._1) / (point._1 - these._1)
-      })
+        point._1 == these._1 match {
+          case true => acc
+          case false => acc * (x - these._1) / (point._1 - these._1)
+        })
     }).sum
   }
 }
